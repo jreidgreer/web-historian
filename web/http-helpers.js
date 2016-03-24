16ff424712b871b7518dfd30d2f9f556dad4d6d4
jreidgreer <jreid.greer@gmail.com>
2016-03-24 11:09:40 -0700

    Serve static files

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -2,18 +2,20 @@ var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
 
-exports.headers = headers = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10, // Seconds.
-  'Content-Type': 'text/html'
+var makeHeaders = function(contentType) {
+  return {
+    'access-control-allow-origin': '*',
+    'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'access-control-allow-headers': 'content-type, accept',
+    'access-control-max-age': 10, // Seconds.
+    'Content-Type': contentType
+  };
 };
 
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
-exports.serveAssets = function(res, asset, callback) { // Might need res argument
+var serveAssets = function(res, asset, callback) { // Might need res argument
   fs.readFile(asset, 'utf8', function(err, result) {
     if (err) {
       callback(err, null);
@@ -23,6 +25,41 @@ exports.serveAssets = function(res, asset, callback) { // Might need res argumen
   });
 };
 
+var sendResponse = function(settings) {
+  // Assign settings to variables
+  res = settings.res;
+  code = settings.code || 200;
+  headers = settings.headers || headers('text/html');
+  content = settings.content || '';
+
+  // Format and send response
+  res.writeHead(code, headers);
+  res.end(content);
+};
+
+var sendStaticResponse = function(res, asset) {
+  serveAssets(res, __dirname + asset, function(err, result) {
+    if (err) {
+      console.log('File failed to load', err);
+      console.log(__dirname + asset);
+      sendResponse(res, 500, 'Oops! We can\'t load that file.');
+    } else {
+      sendResponse({
+        res: res,
+        code: 200,
+        headers: makeHeaders('text/' + path.extname(asset).substring(1)),
+        content: result
+      });
+    }
+  });
+};
+
+module.exports = {
+  headers: makeHeaders,
+  serveAssets: serveAssets,
+  sendResponse: sendResponse,
+  sendStaticResponse: sendStaticResponse
+};
 
 
 // As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -3,34 +3,17 @@ var archive = require('../helpers/archive-helpers');
 var requestHelpers = require('./http-helpers.js');
 // require more modules/folders here!
 
-var sendResponse = function(res, code, message) {
-  res.writeHead(code, requestHelpers.headers);
-  res.end(message);
-};
 
-var sendStaticResponse = function(res, asset) {
-  requestHelpers.serveAssets(res, __dirname + asset, function(err, result) {
-    if (err) {
-      console.log('File failed to load', err);
-      console.log(__dirname + asset);
-      sendResponse(res, 500, 'Oops! We can\'t load that file.');
-    } else {
-      res.writeHead(code, requestHelpers.headers);
-      // res.headers['Content-Type']
-      res.end(message);
-    }
-  });
-};
 
 exports.handleRequest = function (req, res) {
   console.log('Receiving ' + req.method + ' from ' + req.url + ' request with a content type of ');
 
   if (req.method === 'GET') {
     if (req.url === '/') {
-      sendStaticResponse(res, '/public/index.html');
+      requestHelpers.sendStaticResponse(res, '/public/index.html');
     }
     if (req.url === '/styles.css') {
-      sendStaticResponse(res, '/public/styles.css');
+      requestHelpers.sendStaticResponse(res, '/public/styles.css');
     }
   } else if (req.method === 'POST') {
     //
