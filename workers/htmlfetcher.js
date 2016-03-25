0569a4b7f865490dcd02d4af72e8a6172d0d4fcb
jreidgreer <jreid.greer@gmail.com>
2016-03-24 19:24:36 -0700

    Set up html fetching capabilities

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,5 +1,23 @@
 var archive = require('../helpers/archive-helpers.js');
-// Use the code in `archive-helpers.js` to actually download the urls
-// that are waiting.
+var path = require('path');
+var fs = require('fs');
 
-//fs.writeFile...
+// Use the code in `archive-helpers.js` to actually download the urls
+// that are waiting.   ¥  §
+
+
+archive.readListOfUrls(function(err, urls) {
+  if (err) {
+    console.error(err);
+    return;
+  } 
+
+  archive.downloadUrls(['http://www.google.com', 'http://www.amazon.com', 'http://www.w3schools.com/jsref/jsref_replace.asp'], function(error, data, url) {
+    var fileName = archive.sanitizeUrl(url);
+    fs.writeFile(path.join(__dirname + '/../archives/sites/' + fileName + '.html'), data, function(err) {
+      if (err) {
+        console.log('broken inside of downloadUrls', err);
+      }
+    });
+  });
+});
